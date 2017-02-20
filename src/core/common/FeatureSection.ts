0365af6ab622820d61eee28e850f758188ff5b88
Rob DeSisto <rob@Robs-MacBook-Pro.local>
2017-02-20 09:22:45 -0800

    under construction

--- src/core/common/FeatureSection.ts
+++ src/core/common/FeatureSection.ts
@@ -7,17 +7,10 @@ export class FeatureSection {
     public staticContent: StaticContent;
 
     constructor() {
-        // TODO: this is all junk
-        this.sectionMenu = [
-            new NavItem('', 'Overview'),
-            new NavItem('section1', 'Section 1'),
-            new NavItem('section2', 'Section 2')
-        ];
-
         this.staticContent = new StaticContent({
-            title: 'Section Header',
+            title: 'Welcome to Pack 122',
             section: 'home',
-            content: '<p>This blob <b>here</b> (including the header above it) will be pulled from the CMS. It can be as long as short as necessary. For pages with with multiple sections, we have the sub-menu to the left. We may want some sort of eye-catching, hi-rez image about this stuff, but we\'ll need to sort out permission, etc. </p>'
+            content: '<p>You have reached the home page of Cub Scout Pack 122 serving Coronado, the Cays, and Imperial Beach. We are currently under construction but anticipate being up by March 1, 2017.</p>'
 
         });
     }
