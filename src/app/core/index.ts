5003455238a5787b8b09a10a567b3fdd91babbb6
Robert.Desisto <rdesisto@zuora.com>
2017-04-16 16:23:54 -0700

    updating to best practices

---
+++ src/app/core/index.ts
@@ -0,0 +1,7 @@
+/* Models */
+export * from './models/NavItem';
+export * from './models/StaticContent';
+
+/* Services */
+export * from './services/ContentfulService';
+export * from './services/DataService';
